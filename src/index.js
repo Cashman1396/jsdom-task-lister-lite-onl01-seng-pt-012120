030ee7983f4fc67fb65d18c8e4b3ceab22b0d2b4
Chris Ashman <chrisashman96@gmail.com>
2020-07-16 21:52:42 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const taskList = new TaskList();
+
+  const newTaskForm = document.getElementById("create-task-form");
+const newTaskDescription = document.getElementById("new-task-description");
+const newTaskPriority = document.getElementById("new-task-priority");
+
+
+  
 });
